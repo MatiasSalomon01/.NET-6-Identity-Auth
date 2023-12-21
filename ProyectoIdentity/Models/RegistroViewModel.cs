de5fc7e1f49f71939b0db07c82a12126a4d60457
Matias Salomon <matias.salomon@clt.com.py>
2023-12-21 14:26:17 -0300

    Agregar modelo para registro

---
+++ ProyectoIdentity/Models/RegistroViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoIdentity.Models;
+
+public class RegistroViewModel
+{
+    [Required(ErrorMessage = "El email es obligatorio")]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "La contraseña es obligatorio")]
+    [StringLength(50, ErrorMessage = "El {0}, debe estar entre al menos {2} caractereres de longitud", MinimumLength = 5)]
+    [DataType(DataType.Password)]
+    [Display(Name = "Contraseña")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "La confirmación contraseña es obligatorio")]
+    [Compare("Password", ErrorMessage = "La contraseña y confirmacion no coinciden")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirmar Contraseña")]
+    public string ConfirmPassword { get; set; }
+    public string Nombre { get; set; }
+    public string Url { get; set; }
+    public int CodigoPais { get; set; }
+    public string Telefono { get; set; }
+
+    [Required(ErrorMessage = "El país es obligatorio")]
+    public string Pais { get; set; }
+    public string Ciudad { get; set; }
+    public string Direccion { get; set; }
+
+    [Required(ErrorMessage = "La fecha de nacimiento es obligatorio")]
+    public DateTime FechaNacimiento { get; set; }
+
+    [Required(ErrorMessage = "El estado es obligatorio")]
+    public bool Estado { get; set; }
+}
